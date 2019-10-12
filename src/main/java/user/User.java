c14e0a092461ecd79fc83f992ea179b0b4a5b0da
Adi Timoce <adriantimoce@gmail.com>
2019-10-12 11:06:48 +0300

    added application properties & dependencies

---
+++ src/main/java/user/User.java
@@ -0,0 +1,63 @@
+package user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer userID;
+
+    private String userName;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int aptNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAptNumber() {
+        return aptNumber;
+    }
+
+    public void setAptNumber(int aptNumber) {
+        this.aptNumber = aptNumber;
+    }
+}
